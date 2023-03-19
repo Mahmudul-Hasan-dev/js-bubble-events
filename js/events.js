e9b1f9aca53154736d861931c7b95381df2b0176
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-19 15:51:30 +0600

    bubble stop

---
+++ js/events.js
@@ -0,0 +1,15 @@
+document.getElementById('item-2').addEventListener('click', function (event) {
+    console.log('item2 clicked');
+    //to stop bubble we have to use stop propagation
+    event.stopPropagation();
+})
+document.getElementById('ul-list').addEventListener('click', function () {
+    console.log('ul-list clicked')
+})
+document.getElementById('list-container').addEventListener('click', function () {
+    console.log('list-container clicked')
+})
+document.getElementById('body').addEventListener('click', function () {
+    console.log('body clicked')
+})
+//to stop bubble we have to use event stop propagation and take event as parameter of function
